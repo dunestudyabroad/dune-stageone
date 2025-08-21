9dd6cdb0ac9b725fc4e088dced43566f499bf852
Jamshadj <jamshadalij2004@gmail.com>
2025-08-21 11:17:26 +0530

    api updated

--- src/components/counter/counts.tsx
+++ src/components/counter/counts.tsx
@@ -7,9 +7,9 @@ function Count() {
     <div className=" bg-[#FEFEFE] flex items-center justify-center w-full">
       <div className="bg-[#FEFEFE] rounded-lg px-8 py-20 w-full">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <Counter end={10} label="Years of Experience" />
-          <Counter end={15} label="Countries" />
-          <Counter end={200} label="Universities" />
+          <Counter end={14} label="Years of Experience" />
+          <Counter end={22} label="Countries" />
+          <Counter end={2000} label="Universities" />
           <Counter end={400} label="Global Admissions" />
         </div>
       </div>
